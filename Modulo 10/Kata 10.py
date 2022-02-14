240a094a3839bfb7d64321617aa6dc2c3d623161
FelipeReyesMi <89532512+FelipeReyesMi@users.noreply.github.com>
2022-02-14 17:52:07 -0600

    Add files via upload

---
+++ Modulo 10/Kata 10.py
@@ -0,0 +1,47 @@
+#
+##### TRACEBACKS
+#try:
+ #open('config.txt')
+#except FileNotFoundError:
+ #print("No se pudo encontrar el archivo")
+
+#def main():
+#    open("/path/to/mars.jpg")
+
+#if __name__ == '__main__':
+#    main()
+
+
+# CONTROLANDO LAS EXCEPCIONES
+
+#Try y Except de los bloques
+#try:
+#    open('config.txt')
+#except FileNotFoundError:
+#     print("No se encuentra tu archivo")
+
+#def main():
+#    try:
+#        configuration = open('config.txt')
+#    except FileNotFoundError:
+#        print("No se encuentra tu archivo")
+
+
+#if __name__ == '__main__':
+#    main()
+
+#def main():
+#    try:
+#        configuration = open('config.txt')
+#    except Exception:
+#        print("No se encuentra tu archivo")
+
+
+#def main():
+#    try:
+#        configuration = open('config.txt')
+#    except FileNotFoundError:
+#        print("Couldn't find the config.txt file!")
+#    except IsADirectoryError:
+#        print("No se encuentra tu archivo")
+
